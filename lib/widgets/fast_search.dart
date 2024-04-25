a79dc128bae79b36036b6389ab672b4521c0779e
jhalto <zobayerarmannadim@gmail.com>
2024-04-25 15:23:19 +0600

    complete

--- lib/widgets/fast_search.dart
+++ lib/widgets/fast_search.dart
@@ -24,53 +24,55 @@ class _FirstSearchDemoState extends State<FirstSearchDemo> {
         ),
         child: Padding(
           padding: const EdgeInsets.only(left: 30,top: 30,right: 25),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Fast Search",style: TextStyle(fontSize: 25),),
-              Text("You can quickly serch quickly\n for whatever you want",style: TextStyle(fontSize: 18),),
-              Padding(
-                padding: EdgeInsets.only(top: 20),
-                child: TextField(
-
-                  decoration: InputDecoration(
-                    hintText: "Search",
-
-                  fillColor: Colors.white,
-                    prefixIcon: Icon(CupertinoIcons.search),
-                    filled: true,
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.white
-                      ),
-
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Fast Search",style: TextStyle(fontSize: 25),),
+                Text("You can quickly serch quickly\n for whatever you want",style: TextStyle(fontSize: 18),),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: TextField(
+                    scrollPhysics: AlwaysScrollableScrollPhysics(),
+                    decoration: InputDecoration(
+                      hintText: "Search",
+            
+                    fillColor: Colors.white,
+                      prefixIcon: Icon(CupertinoIcons.search),
+                      filled: true,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
                           color: Colors.white
+                        ),
+            
+                        borderRadius: BorderRadius.circular(15),
                       ),
-
-                      borderRadius: BorderRadius.circular(15),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.white
+                        ),
+            
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      // border:  OutlineInputBorder(
+                      //
+                      //
+                      //   borderRadius: BorderRadius.circular(15),
+                      // ),
+                      // enabledBorder: OutlineInputBorder(
+                      //
+                      //
+                      //     borderRadius: BorderRadius.circular(15),
+                      // ),
+                      // focusedBorder: OutlineInputBorder(
+                      //   gapPadding: 17,
+                      //   borderRadius: BorderRadius.circular(15)
+                      // )
                     ),
-                    // border:  OutlineInputBorder(
-                    //
-                    //
-                    //   borderRadius: BorderRadius.circular(15),
-                    // ),
-                    // enabledBorder: OutlineInputBorder(
-                    //
-                    //
-                    //     borderRadius: BorderRadius.circular(15),
-                    // ),
-                    // focusedBorder: OutlineInputBorder(
-                    //   gapPadding: 17,
-                    //   borderRadius: BorderRadius.circular(15)
-                    // )
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
